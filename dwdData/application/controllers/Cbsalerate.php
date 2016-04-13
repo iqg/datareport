b70b7b3585620d9c77939a1828af070fb1b497c6
717540722@qq.com <717540722@qq.com>
2016-04-13 15:45:05 +0800

    no message

--- dwdData/application/controllers/Cbsalerate.php
+++ dwdData/application/controllers/Cbsalerate.php
@@ -25,7 +25,7 @@ class cbSaleRateController extends BasicController{
         $columns = array(
             0 =>'city',
             1 => 'shop',
-            2=> 'order_count',
+            2=> '订单量',
             3=> 'stock',
             4=> 'sale_rate',
             5=> 'saler'
@@ -40,7 +40,7 @@ class cbSaleRateController extends BasicController{
         $orderDir =$requestData['order'][0]['dir'];
 
         $this->cbSaleRate = $this->load('cbSaleRate');
-        $cbSaleRateArray = $this->cbSaleRate->getSale($startDate,$endDate,"",$orderColumn,$orderDir);
+        $cbSaleRateArray = $this->cbSaleRate->getSale($startDate,$endDate,$search,$orderColumn,$orderDir,$start,$length);
 
         $m = new MongoClient("mongodb://iqg_prod:oq9ghGYj9ViR@10.132.163.91:27017/iqg_prod");
         $db = $m->iqg_prod;
@@ -67,61 +67,20 @@ class cbSaleRateController extends BasicController{
             $val['sale_rate'] = $exist ? $val['订单量'] / $stockArray[$val['活动ID']]['stock'] : '不匹配';
             $val['stock'] = $exist ? $stockArray[$val['活动ID']]['stock'] : '不匹配';
         }
-        print_r($cbSaleRateArray);
-        exit;
-        $collectionOrder = $db->campaignOrder;
-        $cursor1 = $collectionOrder->find($query);
-        $orderArray = array();
-        foreach($cursor1 as $id=>$val){
-            foreach($val['data'] as $key=>$val){
-                if(array_key_exists($val['活动ID'],$orderArray)){
-                    $orderArray[$val['活动ID']]['订单量'] += $val['订单量'];
-                }else{
-                    $data = array();
-                    $data['order_count'] = $val['订单量'];
-                    $data['shop'] = $val['shop'];
-                    $data['city'] = $val['city'];
-                    $data['saler'] = $val['saler'];
-                    $data['item'] = $val['item'];
-                    $orderArray[$val['活动ID']] = $data;
-                }
-            }
-        }
 
         $jsonArray = array();
-        if(!empty($search)) {
-            foreach ($orderArray as $key => $val) {
-                if($val['city'] == $search) {
-                    $exist = array_key_exists($key, $stockArray);
-                    $val['sale_rate'] = $exist ? $val['order_count'] / $stockArray[$key]['stock'] : '不匹配';
-                    $val['stock'] = $exist ? $stockArray[$key]['stock'] : '不匹配';
-                    $jsonArray[] = $val;
-                }
-            }
-        }else {
-            foreach ($orderArray as $key => $val) {
-                $exist = array_key_exists($key, $stockArray);
-                $val['sale_rate'] = $exist ? $val['order_count'] / $stockArray[$key]['stock'] : '不匹配';
-                $val['stock'] = $exist ? $stockArray[$key]['stock'] : '不匹配';
-                $jsonArray[] = $val;
-
-            }
+        foreach ($cbSaleRateArray as $key => $val) {
+            $exist = array_key_exists($val['活动ID'],$stockArray);
+            $val['sale_rate'] = $exist ? $val['订单量'] / $stockArray[$val['活动ID']]['stock'] : '不匹配';
+            $val['stock'] = $exist ? $stockArray[$val['活动ID']]['stock'] : '不匹配';
+            $jsonArray[] = $val;
         }
-        if($orderDir == "asc") {
-            usort($jsonArray, function ($a, $b) {
-                return $a['$orderColumn'] > $b['$orderColumn'] ? 1 : -1;
-            });
-        }else{
-            usort($jsonArray, function ($a, $b) {
-                return $b['$orderColumn'] > $a['$orderColumn'] ? 1 : -1;
-            });
-        }
-        $total = count($jsonArray);
-        $jsonArray = array_slice($jsonArray,$start,$length);
+        print_r($jsonArray);
+        exit;
         $json_data = array(
             "draw"            => intval( $this->getParam('draw')),
-            "recordsTotal"    => intval( $total ),
-            "recordsFiltered" => intval( $total),
+            "recordsTotal"    => intval( 100000 ),
+            "recordsFiltered" => intval( 100000),
             "data"            => $jsonArray
         );
 
